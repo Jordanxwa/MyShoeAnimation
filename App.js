667a9d129b6651b353522cfa4b026a508177f43a
Jordan Wallace-Addai <you@example.com>
2019-10-11 00:04:17 -0400

    commit animation

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
   Image,
   View,
   ScrollView,
+  Animated,
   Platform,
   NativeModules
 } from 'react-native';
@@ -15,37 +16,132 @@ if (Platform.OS === 'android') {
     UIManager.setLayoutAnimationEnabledExperimental(true);
   }
 }
-
 export default class App extends React.Component {
+  animation = new Animated.Value(0);
+  animationSpring = new Animated.Value(1);
+  animationSpring2 = new Animated.Value(1);
+  animationSpring3 = new Animated.Value(1);
+
+  componentDidMount() {
+    Animated.timing(this.animation, {
+      duration: 1500,
+      toValue: 1
+    }).start();
+
+    Animated.spring(this.animationSpring, {
+      toValue: 9,
+      speed: 0,
+      delay: 2000,
+      bounciness: 50
+    }).start();
+
+    Animated.spring(this.animationSpring2, {
+      toValue: 9,
+      speed: 0,
+      delay: 2500,
+      bounciness: 50
+    }).start();
+
+    Animated.spring(this.animationSpring3, {
+      toValue: 9,
+      speed: 0,
+      delay: 3100,
+      bounciness: 50
+    }).start();
+  }
+
   render() {
     return (
-      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
-        <Image
-          style={styles.myLogo}
+      <ScrollView
+        contentContainerStyle={{
+          flexGrow: 1
+        }}
+      >
+        <Animated.Image
+          style={{
+            ...styles.myLogo,
+            transform: [
+              {
+                translateY: this.animation.interpolate({
+                  inputRange: [0, 1],
+                  outputRange: [-80, 0]
+                })
+              }
+            ],
+            opacity: this.animation
+          }}
           source={require('./assets/myshoeLogo.png')}
         />
 
-        <Image
-          style={styles.adidasShoe}
+        <Animated.Image
+          style={{
+            ...styles.adidasShoe,
+
+            transform: [
+              {
+                rotate: this.animation.interpolate({
+                  inputRange: [0, 3],
+                  outputRange: ['0deg', '40deg']
+                })
+              }
+            ]
+          }}
           source={require('./assets/images/adidas-kids-grade-school-shoes.png')}
         />
         <Text style={styles.titleText}>Adidas School Kid Shoes</Text>
-        <Text style={styles.shoePrice}>$69.99</Text>
+        <Animated.Text
+          style={{
+            ...styles.shoePrice,
+            left: this.animationSpring
+          }}
+        >
+          $69.99
+        </Animated.Text>
 
-        <Image
-          style={styles.vansShoe}
+        <Animated.Image
+          style={{
+            ...styles.vansShoe,
+
+            transform: [
+              {
+                rotate: this.animation.interpolate({
+                  inputRange: [0, 3],
+                  outputRange: ['0deg', '40deg']
+                })
+              }
+            ]
+          }}
           source={require('./assets/images/original-classics-vans.png')}
         />
         <Text style={styles.titleText}>Original Classic Vans</Text>
-        <Text style={styles.shoePrice}>$59.99</Text>
+        <Animated.Text
+          style={{ ...styles.shoePrice, left: this.animationSpring2 }}
+        >
+          $59.99
+        </Animated.Text>
 
-        <Image
-          style={styles.nikeShoe}
+        <Animated.Image
+          style={{
+            ...styles.nikeShoe,
+
+            transform: [
+              {
+                rotate: this.animation.interpolate({
+                  inputRange: [0, 3],
+                  outputRange: ['0deg', '40deg']
+                })
+              }
+            ]
+          }}
           source={require('./assets/images/nike-free-sneakers-nike-air-max.png')}
         />
 
         <Text style={styles.titleText}>Nike Air Max</Text>
-        <Text style={styles.shoePrice}>$79.99</Text>
+        <Animated.Text
+          style={{ ...styles.shoePrice, left: this.animationSpring3 }}
+        >
+          $79.99
+        </Animated.Text>
       </ScrollView>
     );
   }
